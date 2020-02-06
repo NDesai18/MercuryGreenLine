c2cd5650d906adef0300a382f6bc7c5d39287caf
NDesai18 <60348023+NDesai18@users.noreply.github.com>
2020-02-06 14:23:30 +0000

    Update ElegooControl.cpp

--- ElegooControl.cpp
+++ ElegooControl.cpp
@@ -1,33 +1,50 @@
 #include "SerialChecker.h"
-#include "MilliTimer.h"
-
+//#include "MilliTimer.h"
+//
 SerialChecker scpc(Serial);
-HardwareSerial s1(1);
+//HardwareSerial s1(1);
+
+const int step_pin = 22;
+const int direc_pin = 21;
 
 void setup()
 {
   scpc.init();
-  scmotor.init();
+//  scmotor.init();
   scpc.setMsgMaxLen(64);
-  scmotor.setMsgMaxLen(64);
-  pinMode(13, OUTPUT); // Initialising the pins here, logic in lab book
-  pinMode(12, OUTPUT);
+//  scmotor.setMsgMaxLen(64);
+  pinMode(direc_pin, OUTPUT); // Initialising the pins here, logic in lab book
+  pinMode(step_pin, OUTPUT);
   pinMode(34, INPUT);
   pinMode(35, INPUT);
+  digitalWrite(direc_pin, HIGH);
+  scpc.println("connected successfully");
 }
 
 void loop()
 {
-//  checkSerialPC();
+    checkSerialPC();
 //  checkEndStop();
 //  printToLCD();
-  runContinous();
+    runContinuous();
 }
 
-runContinuous()
+void runContinuous()
 {
-  digitalWrite(13, HIGH);
-  delayMicroseconds(500);
-  digitalWrite(13, LOW);
-  delayMicroseconds(500);
+  digitalWrite(step_pin, HIGH);
+  delay(5);
+  digitalWrite(step_pin, LOW);
+  delay(5);
+}
+
+void checkSerialPC()
+{
+  if (scpc.contains("H")){
+    scpc.println("Going up");
+    digitalWrite(direc_pin, HIGH);
+  }
+  else if (scpc.contains("L")){
+    scpc.println("Going down");
+    digitalWrite(direc_pin, LOW);
+  }
 }
